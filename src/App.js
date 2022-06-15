f6ea882ac859246a3dabfd0addbf51d12eb634b5
Ethan Saxenian <esaxenian@middlebury.edu>
2022-06-15 14:44:40 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import Plot from 'react-plotly.js';
 
-function App() {
+export default function App() {
+  const data = [...Array(256).keys()].filter((x) => x % 10 == 0)
+
+  const product = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+
+  const points = product(data, data, data);
+
+  const x = []
+  const y = []
+  const z = []
+
+  points.forEach(([x1, y1, z1]) => {
+    x.push(x1)
+    y.push(y1)
+    z.push(z1)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Plot
+        style={{border: '1px solid black'}}
+        data={[
+          {
+            x,
+            y,
+            z,
+            type: 'scatter3d',
+            mode: 'markers',
+            marker: {
+              color: points.map(([r, g, b]) => `rgb(${r}, ${g}, ${b})`),
+            }
+          }
+        ]}
+        layout={{
+          width: 800,
+          height: 800,
+          margin: {
+            l: 0,
+            r: 0,
+            t: 0,
+            b: 0
+          },
+          scene: {
+            xaxis: {
+              title: 'Red'
+            },
+            yaxis: {
+              title: 'Green'
+            },
+            zaxis: {
+              title: 'Blue'
+            },
+            camera: {
+              eye: {
+                x: 1.6,
+                y: 1.6,
+                z: 1.6
+              }
+            }
+          }
+        }}
+      />
     </div>
   );
 }
-
-export default App;
